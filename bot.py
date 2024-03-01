be81a35565d1c81a17cbad714f855cb408c6ac4d
KerinaLerida <kerencouton@gmail.com>
2024-03-01 21:25:32 +0100

    Update : Tout marche !

--- bot.py
+++ bot.py
@@ -1,14 +1,17 @@
 import discord
 from database import SimpleSQLiteDatabase
+from discord import app_commands
 import logging
-import responses
-import asyncio
 import threading
+import common
 import time
 
-#Update signal is produced each n seconds and champion is updated ONLY when ?guess command is called
-n=3600
+#INIT VARS--------------------------------------
+n=3600 # Update signal is produced each n seconds and champion is updated ONLY when ?guess command is called
 choose_update=False
+client = discord.Client(intents=discord.Intents.all())
+tree=app_commands.CommandTree(client)
+#-----------------------------------------------
 
 def update_bool_guess(my_database):
     global choose_update
@@ -16,89 +19,27 @@ def update_bool_guess(my_database):
         choose_update=True
         time.sleep(n)
 
-"""async def update_bool_guess(my_database):
-    hours,minutes =responses.how_long_guess_mode_active()
-    while True:
-        await asyncio.sleep(60)
-        if hours<=0 and minutes<=0:
-            responses.guess_mode(responses.hours_x,my_database)"""
-
-def check_role(name_role, msg):
-    verif_role = discord.utils.get(msg.guild.roles, name=name_role)
-    if verif_role in msg.author.roles:
-        return True
-    return False
-
-async def send_embed_message_with_file(message, embed, file):
-    try:
-        await message.channel.send(embed=embed, file=file)
-    except Exception as e:
-        print(e)
-        logging.error(e)
-
-# Send messages as embeds
-async def send_embed_message(message, embed):
-    try:
-        await message.channel.send(embed=embed)
-    except Exception as e:
-        print(e)
-        logging.error(e)
-
-# Send messages
-async def send_message(message, user_message, my_database):
-    try:
-        response = responses.handle_response(message, user_message, my_database)
-        if isinstance(response, tuple) and len(response) == 2:
-            embed, file = response
-            await send_embed_message_with_file(message, embed, file)
-        elif isinstance(response, discord.Embed):
-            await send_embed_message(message, response)
-        elif response:
-            await message.channel.send(response)
-
-    except Exception as e:
-        print(e)
-        logging.error(e)
-
-
 def run_discord_bot():
-    TOKEN = 'MTE0MDkyNTM5MDE3NzY5Nzc5Mg.G2FNf5.a3P8RME9KnMK_oD0k8lWsmQTwjIatOfja-S4z0'
-    client = discord.Client(intents=discord.Intents.all())
     my_database = SimpleSQLiteDatabase('champions.db')
+    common.DB=my_database
     logging.basicConfig(filename='bot.log', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
     @client.event
     async def on_ready():
+        await tree.sync(guild=discord.Object(id=common.SERVER_ID))
         print(f'{client.user} is now running!')
         logging.info(f'{client.user} is now running!')
 
-    @client.event
-    async def on_message(message):
-        # Make sure bot doesn't get stuck in an infinite loop
-        if message.author == client.user:
-            return
-
-        # ******************** DEBUG ********************
-        # Get data about the user
-        username = str(message.author)
-        user_message = str(message.content)
-        channel = str(message.channel)
-        id_user = str(message.author.id)
-
-        # ************************************************
-
-        if message.channel.name == "🤖╿darkinia":
-            if check_role("Cotisant", message):  # Vérifie : Role Cotisant nécessaire
-                if user_message[0] == '?':
-                    user_message = user_message[1:]  # [1:] Removes the '?'
-                    await send_message(message, user_message, my_database)
-                else:
-                    return
+        logging.info("List of commands:")
+        logging.info(len(tree.get_commands()))
+        for command in tree.get_commands():
+            logging.info(command.name)
 
     # Lancez la tâche périodique
     th = threading.Thread(target=update_bool_guess, args=(my_database,))
-    th.start();
+    th.start()
 
     # Remember to run your bot with your personal TOKEN
-    client.run(TOKEN)
+    client.run(common.BOT_TOKEN)
+
 
--- database.py
+++ database.py
@@ -18,12 +18,12 @@ class SimpleSQLiteDatabase:
             self.connection.execute(create_users_table_query)
 
     # Users Table
-    def insert_user(self, id_user):
+    def insert_user(self, id_user, name):
         insert_query = '''
-            INSERT INTO users (id,name,points) VALUES (?,"",0)
+            INSERT INTO users (id,name,points) VALUES (?,?,0)
         '''
         with self.connection:
-            self.connection.execute(insert_query, (id_user,))
+            self.connection.execute(insert_query, (id_user,name))
 
     def update_points_by_id(self, ajout, id_user):
         update_query = '''
@@ -56,11 +56,11 @@ class SimpleSQLiteDatabase:
             if result:
                 return str(result[0])
             else:
-                return None  # Ou tout autre indication de l'absence de résultat
+                return None
 
     def get_all_users(self):
         select_all_query = '''
-            SELECT id, name, points
+            SELECT name, points
             FROM users ORDER BY points DESC
         '''
         with self.connection:
