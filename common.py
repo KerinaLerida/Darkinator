90fa38e552eb7b57dc6c8f978c715870ba742a8e
KerinaLerida <kerencouton@gmail.com>
2024-03-02 18:00:43 +0100

    Update : Dockerfile & Conteneurisation

--- common.py
+++ common.py
@@ -1,6 +1,6 @@
-BOT_TOKEN                       = "MTE0MDkyNTM5MDE3NzY5Nzc5Mg.Gacy3R.auMvDAdixNrJR1GOjdawCpaHAMt8Hpe6F1H7ck"
-SERVER_ID                       = 884026358387335218
-DEFAULT_CHANNEL_ID              = 1142777699404894309
+BOT_TOKEN                       = "METTRE SON TOKEN ICI"
+SERVER_ID                       = 0
+DEFAULT_CHANNEL_ID              = 0
 ADMIN_ROLE                      = "Bureau"
 
 #DONT EDIT THE FOLLOWING!
